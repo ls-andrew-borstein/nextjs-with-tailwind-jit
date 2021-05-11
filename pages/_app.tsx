a5238944555338142d305bbfb8fcaf3ec0727ce4
Andrew Borstein <andrew.borstein@lightspeedhq.com>
2021-05-11 10:21:57 -0700

    Add responsive viewport meta tag to <App />

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 import { AppProps } from "next/app";
 
 const NextApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default NextApp;
