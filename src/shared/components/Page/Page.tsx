cb73bebd8ba63cde07d1ddb77f7ff605b67cca57
Andrew Borstein <andrew.borstein@lightspeedhq.com>
2021-05-12 08:58:51 -0700

    Move order of Page back link (so it's first)

--- src/shared/components/Page/Page.tsx
+++ src/shared/components/Page/Page.tsx
@@ -21,13 +21,13 @@ const Page: React.FC<PageProps> = ({ children, title }) => {
       <div className="min-h-screen flex flex-col">
         <main className="flex-1 flex flex-col justify-center items-center align-middle sm:max-w-3xl sm:mx-auto p-3 text-center">
           <h1 className="text-4xl sm:text-6xl font-bold">{title}</h1>
-          {children}
           <Link
             className="absolute top-3 left-3 font-semibold hover:text-blue-900 p-3 rounded text-blue-600 underline"
             href="/"
           >
             &larr; Back to Home
           </Link>
+          {children}
         </main>
         <Footer />
       </div>
