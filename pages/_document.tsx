b942d6aaf808810c6aa62302b0c67919a5cc6918
Andrew Borstein <andrew.borstein@lightspeedhq.com>
2021-08-17 15:21:53 -0400

    Add custom Google font
    Should be auto-optimized: https://nextjs.org/docs/basic-features/font-optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
