faff146b2ac0dfe8535f64a3929f1d8fefb1988e
Andrew Borstein <andrew.borstein@lightspeedhq.com>
2021-08-17 13:51:57 -0400

    <InfoCard> - update styling

--- src/shared/components/InfoCard/InfoCard.tsx
+++ src/shared/components/InfoCard/InfoCard.tsx
@@ -9,7 +9,11 @@ interface InfoCardProps {
 const InfoCard: React.FC<InfoCardProps> = ({ description, title, url }) => {
   return (
     <Link
-      className="p-6 text-left border rounded-xl hover:text-blue-600 focus:text-blue-600"
+      className="
+        border focus:border-blue-600 rounded-xl outline-none
+        p-6 space-y-3
+        text-left hover:text-blue-600
+      "
       href={url}
     >
       <h2 className="text-2xl font-bold">{title} &rarr;</h2>
