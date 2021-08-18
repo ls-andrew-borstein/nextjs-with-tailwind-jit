bd33d1f5c6744fa62a4650cfd8654c6aa1818cde
Andrew Borstein <andrew.borstein@lightspeedhq.com>
2021-08-18 17:28:24 -0400

    fix font script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,9 +21,8 @@ class AppDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            as="style"
-            href="https://fonts.googleapis.com/css2?family=Open+Sans&display=optional"
-            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+            rel="stylesheet"
           />
         </Head>
         <body>
